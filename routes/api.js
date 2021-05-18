e09ca142ab4bd4ef5a3cf0d1482a4ffbf391b050
Heather Storseth <heatherstorseth3@gmail.com>
2021-05-18 12:31:49 -0400

    updated routes

--- routes/api.js
+++ routes/api.js
@@ -1,26 +1,48 @@
 const router = require('express').Router();
 const Tracker = require('../models/tracker.js');
 
-router.post('/api/tracker', ({ body }, res) => {
-    Tracker.create(body)
+router.get('/api/workouts/range', (req, res) => {
+    Tracker.aggregate([
+        {$addFields: {
+            totalDuration:{
+                $sum: '$exercises.duration'
+            }
+        }}
+    ]).sort({_id: -1}).limit(7)
     .then(dbTracker => {
         res.json(dbTracker);
     })
+    .catch(err => res.status(500).json(err))
 });
 
-router.post('/api/tracker/bulk', ({ body }, res) => {
-    Tracker.insertMany(body)
+router.get('/api/workouts', (req, res) => {
+    Tracker.aggregate([
+        {$addFields: {
+            totalDuration:{
+                $sum: '$exercises.duration'
+            }
+        }}
+    ])
     .then(dbTracker => {
         res.json(dbTracker);
     })
+    .catch(err => res.status(500).json(err))
+})
+
+router.post('/api/workouts', (req, res) => {
+    Tracker.create({})
+    .then(dbTracker => {
+        res.json(dbTracker);
+    })
+    .catch(err => res.status(500).json(err))
 });
 
-router.get('/api/tracker/bulk', (req, res) => {
-    Tracker.find({})
-    .sort({ date: -1 })
+router.put('/api/workouts/:id', (req, res) => {
+    Tracker.findByIdAndUpdate(req.params.id, {$push:{exercises:req.body}}, {new: true, runValidators: true})
     .then(dbTracker => {
         res.json(dbTracker);
     })
+    .catch(err => res.status(500).json(err))
 });
 
 module.exports = router;
